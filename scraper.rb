9ac2d212c5fe3aa685e9efbca9accd3f32d592d0
April Rabkin <april.rabkin@gmail.com>
2014-09-23 14:27:08 -0400

    scraped and parsed website

--- scraper.rb
+++ scraper.rb
@@ -11,37 +11,43 @@ class Scraper
 	end
 
 	def scrape_each_county_page
-		i = 1	
+		i = 0
 		while i < 159 do #The last one is Worth County
 			agent = Mechanize.new 
 			page = agent.get("http://sos.ga.gov/cgi-bin/countyregistrarsindex.asp")
 			form = page.forms[1]
 			form.field_with(:name=>"CountyName").options[i].click
-#			form.submit
-						binding.pry
-#			@table = form.submit.parser.css('.subBody elections')
-
-			i += 1
+			button = page.forms[1].buttons[0]
+			newpage = agent.submit(form, button)
+			@table = newpage.parser.css('.subBody')
 			parse_county
+			i += 1
 		end
 	end
 
 	def parse_county
-		if @table.children[0].text.include?("County County")
-			names = @table.children[0].text.gsub!("County County", "County, County").split!(",") #works for everything except Kansas and2 St Louis
-			county = names[0]
-			office = names[1]
-		else 
-			county = @table.children[0].text
+		table = @table.inner_html.gsub(/\n|\r|\t/,'').split('<br>').map do |el| 
+			el.split('<hr>') 
+		end.flatten
+		if table[1]
+			if table[1].include?("County")
+				county_name = table[1].gsub(/County /,'County, ').split(',')[0]
+				office = table[1].gsub(/County /,'County, ').split(',')[1].strip
+			end
 		end
-		phone = @table.children[6].text
-		if @table.children.attribute('href').text != ""
-			website = @table.children.attribute('href').text
+		phone = (@table.text.scan /Telephone: \(\d*\)\s?\d*-\d*.*/).map do |p| p.gsub(/Telephone: |\n|\r|\t/,'') end
+		website = (@table.text.scan /Website: .*/)
+		if website
+			website.map! do |w| 
+				w.gsub('Website: ', '')
+			end
 		end
-		@rows << [county, office, phone, website ? website : nil].flatten
-	end
+		@rows << [county_name, office, phone, website].flatten!	
+		binding.pry	
+	end			
 
 	def write_into_CSV_file
+
 		CSV.open("spreadsheet.csv", "wb") do |csv|
 			@rows.map do |line|
 				csv << line
